8ecea34b6004e0e2bef8272e12fc279a66e84787
dr-chain <12458730+dr-chain@users.noreply.github.com>
2025-07-06 22:03:53 +0530

    largeblob, prf

--- app/src/main/java/org/passkey/MainActivity.kt
+++ app/src/main/java/org/passkey/MainActivity.kt
@@ -3,6 +3,7 @@ package org.passkey
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Base64
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,7 @@ import com.yubico.yubikit.fido.client.BasicWebAuthnClient
 import com.yubico.yubikit.fido.ctap.Ctap2Session
 import com.yubico.yubikit.fido.webauthn.PublicKeyCredential
 import com.yubico.yubikit.fido.webauthn.PublicKeyCredentialCreationOptions
+import com.yubico.yubikit.fido.webauthn.PublicKeyCredentialRequestOptions
 import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URL
@@ -23,12 +25,14 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     private lateinit var yubiKitManager: YubiKitManager
+    private lateinit var rawId: String
+    private lateinit var infoView: TextView
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val infoView = TextView(this).apply {
+        infoView = TextView(this).apply {
             text = "Insert or tap your YubiKey to register a passkey..."
             textSize = 16f
             setPadding(32, 300, 32, 32)
@@ -52,10 +56,26 @@ class MainActivity : AppCompatActivity() {
                     } catch (e: Exception) {
                         e.printStackTrace()
                         runOnUiThread {
+                            Toast.makeText(
+                                this, "Connection failed: ${e.message}", Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                }
+
+                /*
+                device.requestConnection(SmartCardConnection::class.java) { result ->
+                    try {
+                        val connection = result.value
+                        authenticateWithYubiKey(connection)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        runOnUiThread {
                             Toast.makeText(this, "Connection failed: ${e.message}", Toast.LENGTH_LONG).show()
                         }
                     }
                 }
+                */
             }
         } catch (e: Exception) {
             e.printStackTrace()
@@ -63,7 +83,10 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "NFC setup failed: ${e.message}", Toast.LENGTH_LONG).show()
             }
         }
+
+
     }
+
 
     private fun registerWithYubiKey(connection: SmartCardConnection) {
         try {
@@ -76,12 +99,15 @@ class MainActivity : AppCompatActivity() {
                 challenge, Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP
             )
 
-            val fakeRequestJson = """
+            val createJson = """
                 {
-                    "publicKey": {
                         "rp": { "name": "Example RP", "id": "example.com" },
                         "user": {
-                            "id": "${Base64.encodeToString("user123".toByteArray(), Base64.NO_WRAP)}",
+                            "id": "${
+                Base64.encodeToString(
+                    "user123".toByteArray(), Base64.NO_WRAP
+                )
+            }",
                             "name": "user@example.com",
                             "displayName": "Example User"
                         },
@@ -89,17 +115,18 @@ class MainActivity : AppCompatActivity() {
                         "pubKeyCredParams": [
                             { "type": "public-key", "alg": -7 }
                         ],
-                        "timeout": 60000,
+                        
                         "authenticatorSelection": {
                             "residentKey": "required",
-                            "userVerification": "preferred"
+                            "userVerification": "required"
                         },
+                        "extensions": {"prf": {}, "largeBlob": {}},
+                        "timeout": 60000,
                         "attestation": "none"
-                    }
                 }
             """.trimIndent()
 
-            val optionsJson = JSONObject(fakeRequestJson).getJSONObject("publicKey")
+            val optionsJson = JSONObject(createJson)
             val options = PublicKeyCredentialCreationOptions.fromMap(optionsJson.toMap())
             val clientData = buildClientData("webauthn.create", origin, challengeB64)
 
@@ -107,18 +134,74 @@ class MainActivity : AppCompatActivity() {
                 clientData, options, URL(origin).host, "1234".toCharArray(), null, null
             )
 
+            rawId = JSONObject(credential.toMap()).getString("rawId")
             val resultJson = JSONObject(credential.toMap()).toString(2)
             runOnUiThread {
-                Toast.makeText(this, "Passkey created:\n$resultJson", Toast.LENGTH_LONG).show()
+                infoView.text = "Passkey created:\n\n$resultJson"
             }
+            Log.i("Passkey", "Passkey created: $resultJson")
+
 
         } catch (e: Exception) {
             e.printStackTrace()
             runOnUiThread {
-                Toast.makeText(this, "Passkey registration failed:\n${e.message}", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this, "Passkey registration failed:\n${e.message}", Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
+
+
+    private fun authenticateWithYubiKey(connection: SmartCardConnection) {
+        try {
+            val session = Ctap2Session(connection)
+            val client = BasicWebAuthnClient(session)
+
+            val origin = "https://example.com"
+            val challenge = ByteArray(32).apply { Random.nextBytes(this) }
+            val challengeB64 = Base64.encodeToString(
+                challenge, Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP
+            )
+
+            //"largeBlob": {"write": "LARGE_BLOB "}
+            val requestJson = """
+                {
+                  "challenge": "$challengeB64",
+                  "timeout": 60000,
+                  "rpId": "example.com",
+                  "userVerification": "required",
+                  "extensions": {
+                  "prf": {"eval": {"first": "SEED"}},
+                  "largeBlob": {"read": true}
+                  }
+                }
+        """.trimIndent()
+
+            //Log.i("Passkey", "Passkey Login Request: $requestJson")
+            val optionsJson = JSONObject(requestJson)
+            val options = PublicKeyCredentialRequestOptions.fromMap(optionsJson.toMap())
+            val clientData = buildClientData("webauthn.get", origin, challengeB64)
+
+            val credential: PublicKeyCredential = client.getAssertion(
+                clientData, options, URL(origin).host, "1234".toCharArray(), null
+            )
+
+            val resultJson = JSONObject(credential.toMap()).toString(2)
+            runOnUiThread {
+                infoView.text = "Passkey Login Successful:\n\n$resultJson"
+            }
+            Log.i("Passkey", "Passkey authenticated: $resultJson")
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            runOnUiThread {
+                Toast.makeText(this, "Authentication failed:\n${e.message}", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+
 
     private fun buildClientData(type: String, origin: String, challengeB64: String): ByteArray {
         val clientDataJson = """
